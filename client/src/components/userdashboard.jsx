a007317d21f9b17859465bc7779d9c348f4c5e19
Jorrel Rajan <jr8867@princeton.edu>
2024-06-13 09:59:53 -0400

    Added Barebones functionality to dashboard

--- client/src/components/userdashboard.jsx
+++ client/src/components/userdashboard.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import UserSession from '../utils/userSession';
+import axios from 'axios';
+
 
 const UserDashboard = () => {
 
     var id = UserSession.getId();
 
+    const [isLoading, setLoading] = useState(true);
+    const [result, setResult] = useState();
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/users/${id}`)
+            .then(response => {
+                setResult(response.data);
+                setLoading(false);
+            });
+    }, [id]);
+    
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <div className="">
+        <div>
             <p>{id}</p>
+            <p>{result.name}</p>
         </div>
     );
 };
